71b9a2d20ff9b14a58c8a61d9ef4b51a0db530e8
Júlia Acosta <juliagabriela98@gmail.com>
2022-10-30 11:44:09 -0300

    final polish on home desktop view

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,34 +1,37 @@
 import { FC } from "react";
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 import * as I from "../../assets/icons";
 import "./styles.scss";
 
 const Footer: FC = () => {
-    return <footer className="footer-container">
-        <section className="begin-section">
-            Essa janela do navegador é usada para manter sua sessão de autenticação ativa. Deixe-a aberta em segundo plano e abra uma nova janela para continuar a navegar.
-        </section>
-        <div className="divider"/>
-        <section className="end-section">
-            <div className="refresh-text">
-                <div>Application refresh in</div>
-                <div className="seconds-text">600 <span>seconds</span></div>
-            </div>
+    return (
+        <footer className="footer-container">
+            <section className="begin-section">
+                Essa janela do navegador é usada para manter sua sessão de
+                autenticação ativa. Deixe-a aberta em segundo plano e abra uma
+                nova janela para continuar a navegar.
+            </section>
+            <div className="divider" />
+            <section className="end-section">
+                <div className="refresh-text">
+                    <div>Application refresh in</div>
+                    <div className="seconds-text">
+                        600
+                        <div>seconds</div>
+                    </div>
+                </div>
 
-            <div className="button-links-container">
-                <div className="secondary-link">
-                    <Link to="">
-                        Acessar Busca
-                    </Link>
+                <div className="button-links-container">
+                    <div className="secondary-link">
+                        <Link to="">Acessar Busca</Link>
+                    </div>
+                    <div className="primary-link">
+                        <Link to="">Logout</Link>
+                    </div>
                 </div>
-                <div className="primary-link">
-                    <Link to="">
-                        Logout
-                    </Link>
-                </div>
-            </div>
-        </section>
-    </footer>;
+            </section>
+        </footer>
+    );
 };
 
 export default Footer;
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,37 +1,51 @@
 import { FC } from "react";
 import Header from "../../components/Header";
-import Footer from "../../components/Footer"
+import Footer from "../../components/Footer";
 
-import "./styles.scss"
-import * as I from '../../assets/icons'
+import "./styles.scss";
+import * as I from "../../assets/icons";
 
 const mission = [
-    {title: "Our mission is", subtitle: "Nossa missão é"},
-    {title: "to transform the world", subtitle: "transformar o mundo"},
-    {title: "building digital experiences", subtitle: "construindo experiências digitais"},
-    {title: "that enable our client’s growth", subtitle: "que permitam o crescimento dos nossos clientes"}
-]
+    { title: "Our mission is", subtitle: "Nossa missão é" },
+    { title: "to transform the world", subtitle: "transformar o mundo" },
+    {
+        title: "building digital experiences",
+        subtitle: "construindo experiências digitais",
+    },
+    {
+        title: "that enable our client’s growth",
+        subtitle: "que permitam o crescimento dos nossos clientes",
+    },
+];
 const Home: FC = () => {
     return (
-        <div className="home-container">
+        <>
             <Header />
-            <div className="home-content">
-               <img src={I.uolBall} alt="oul ball"/>
-               <div className="mission">
-                    {mission.map(({title, subtitle}, idx) => (
-                        <>
-                            <div className={idx === 0? "small-mission-title" : "mission-title"}>
-                                {title}
-                            </div>
-                            <div className="mission-subtitle">
-                                {subtitle}
-                            </div>
-                        </>
-                    ))}
-                </div>  
+            <div className="home-container">
+                <div className="home-content">
+                    <img src={I.uolBall} alt="oul ball" />
+                    <div className="mission">
+                        {mission.map(({ title, subtitle }, idx) => (
+                            <>
+                                <div
+                                    className={
+                                        idx === 0
+                                            ? "small-mission-title"
+                                            : "mission-title"
+                                    }
+                                >
+                                    {title}
+                                </div>
+                                <div className="mission-subtitle">
+                                    {subtitle}
+                                </div>
+                            </>
+                        ))}
+                    </div>
+                </div>
+                <Footer />
             </div>
-            <Footer /> 
-        </div>
+        </>
     );
 };
 
