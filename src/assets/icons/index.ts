b8899b5ce29a22bd1fd0c1b11e9943ff5653ae56
Julia <juliagabriela98@gmail.com>
2022-10-30 10:02:54 -0300

    add home screen

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -3,5 +3,6 @@ import passwordIcon from "./icon-password.svg";
 import compassLogo from "./compass-logo.svg";
 import compassLogoBlack from "./compass-logo-black.svg";
 import climate from "./cloudy.svg";
+import uolBall from "./uol-ball.svg"
 
-export { userIcon, passwordIcon, compassLogo, compassLogoBlack, climate };
+export { userIcon, passwordIcon, compassLogo, compassLogoBlack, climate, uolBall };
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,12 +5,15 @@ import "./styles.scss";
 const Header: FC = () => {
     return (
         <header className="header-container">
-            <img src={I.compassLogoBlack} alt="Compass Logo" />
-            <h1>11:26</h1>
+            <img height={86} width={300} src={I.compassLogoBlack} alt="Compass Logo" />
+            <div className="header-time">
+                <h1>11:26</h1>
+                <div>terça-feira, 17 de março de 2020</div>
+            </div>
             <div>
                 <div className="climate-text">Passo Fundo - RS</div>
                 <div className="climate-container">
-                    <img src={I.climate} alt="Compass Logo" />
+                    <img src={I.climate} alt="climate cloudy icon" />
                     <div className="climate-text-number">22º</div>
                 </div>
             </div>
